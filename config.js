9980df0f40556609612c6151d5511ffec8e2df41
217105922 <41194466+217105922@users.noreply.github.com>
2022-05-10 06:49:00 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+exports.config = {
+  host: "satao.db.elephantsql.com",
+  port: 5432,
+  user: "vthbjfiq",
+  password: "hbmsRP2ejXNeKFEiLfZtlk1AkdHUiEvY",
+  database: "vthbjfiq",
+  connection_limit: 100
+}
+
+exports.configMongo = {
+  user: 'admin',
+  pwd:  'wEVvGbLkX34Lr07d',
+  host: 'cluster0.s5npo.mongodb.net',
+  dbname: 'mongo'
+}
